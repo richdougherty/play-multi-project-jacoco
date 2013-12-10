3127bc8a5955d32458ba4770519b201136734d27
muymoo <matt.momont@gmail.com>
2013-12-10 14:35:13 -0800

    Initial Commit

---
+++ modules/common/test/ApplicationTest.java
@@ -0,0 +1,47 @@
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import play.mvc.Content;
+import play.test.FakeApplication;
+import play.test.Helpers;
+
+
+/**
+*
+* Simple (JUnit) tests that can call all parts of a play app.
+* If you are interested in mocking a whole application, see the wiki for more details.
+*
+*/
+public class ApplicationTest {
+
+    @Test
+    public void simpleCheck() {
+        int a = 1 + 1;
+        assertThat(a).isEqualTo(2);
+    }
+
+    @Test
+    public void renderTemplate() {
+        Content html = views.html.index.render("Your new application is ready.");
+        assertThat(contentType(html)).isEqualTo("text/html");
+        assertThat(contentAsString(html)).contains("Your new application is ready.");
+    }
+
+    @Test
+    public void workingDirectoryShouldBeConstant() {
+        FakeApplication app = new FakeApplication(new java.io.File("."),
+                Helpers.class.getClassLoader(), new HashMap<String, String>(), new ArrayList<String>(), null);
+        
+        File testData = app.getWrappedApplication().getFile("./test/resources/test-data.txt");
+        
+        assertThat(testData).isFile();
+    }
+
+}
---
+++ project/Build.scala
@@ -0,0 +1,62 @@
+import sbt._
+import play.Project._
+import Keys._
+import de.johoop.jacoco4sbt.JacocoPlugin._
+
+
+object ApplicationBuild extends Build {
+
+  val appVersion = "1.0-SNAPSHOT"
+
+    
+  // So we can get code coverage
+  lazy val jacocoSettings = Seq(
+      parallelExecution in jacoco.Config := false,
+      jacoco.excludes in jacoco.Config := Seq("Routes*","*Reverse*","*anonfun*")//,
+      
+      // Workaround 
+//      Keys.fork in jacoco.Config := true,
+
+      // Workaround to run jacoco in current working directory
+//      testGrouping in jacoco.Config := {
+//        val original: Seq[Tests.Group] = (testGrouping in jacoco.Config).value
+//
+//        original.map { group =>
+//          val forkOptions = ForkOptions(
+//            bootJars = Nil,
+//            javaHome = javaHome.value,
+//            connectInput = connectInput.value,
+//            outputStrategy = outputStrategy.value,
+//            runJVMOptions = javaOptions.value,
+//            workingDirectory = Some(baseDirectory.value), // Run jacoco in current subproject directory
+//            envVars = envVars.value
+//          )
+//
+//          group.copy(runPolicy = Tests.SubProcess(forkOptions))
+//        }
+//      }
+    )
+    
+  lazy val s = playJavaSettings ++ Seq(jacoco.settings:_*) ++ jacocoSettings
+  
+  val commonDependencies = Seq(
+      javaCore
+      )
+      
+  val common = play.Project(
+      "common", appVersion, commonDependencies, settings = s, path = file("modules/common")    
+  ) 
+    
+  val appDependencies = Seq(
+      javaJdbc,
+      javaEbean,
+      cache
+   )
+   
+   val main = play.Project("play-multi-project-jacoco", appVersion, appDependencies, settings = s).dependsOn(
+      common
+   ).aggregate(
+      common    
+   )
+}
+   
